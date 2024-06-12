c1a76c308dfa0e82d66ba71170447bad56e1a277
Generosa Litton <glcodeworks@gmail.com>
2024-06-11 20:30:51 -0500

    study sessions

---
+++ JS109review.js
@@ -0,0 +1,53 @@
+// let a = 4;
+// let b = a;
+// a = 7;
+// console.log(b);
+
+// let num = 1;
+
+// while (num < 3) {
+//   let num = 5;
+//   num += 1;
+// }
+
+// console.log(num);
+
+let word = "hello";
+let newWord = word.replace("h", "j");
+
+// console.log(newWord); // 'jello', old word var doesn't change
+
+// let myWord = "Hello";
+// let otherWord = " there";
+// let finalWord = myWord.concat(otherWord);
+
+// console.log(finalWord);
+
+// let myWord = "Hello";
+// myWord.repeat(3);
+// console.log(myWord);
+// myWord.replace("H", "J");
+// console.log(myWord);
+// myWord.toUpperCase();
+// console.log(myWord);
+
+// let myWords = ["Hello"];
+// myWords.push("Goodbye");
+
+// console.log(myWords);
+
+// let myWords = ["Hello"];
+// myWords.concat(["Goodbye"]);
+
+// console.log(myWords);
+
+// let myWords = ["Hello"];
+// let finalWords = myWords[0].toUpperCase();
+
+// console.log(myWords);
+// console.log(typeof finalWords);
+
+let myWords = ["Hello"];
+myWords[0] = myWords[0].toUpperCase();
+
+console.log(myWords);
